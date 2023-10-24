ba722fe4d26791ffe40e796b00356f7d195ee714
Abhishek Singh <abhis1997singh@gmail.com>
2023-10-24 00:46:09 -0400

    sharing images

--- get_relevant_page.py
+++ get_relevant_page.py
@@ -1,14 +1,54 @@
 import urllib.parse
+import requests
+from io import BytesIO
+import fitz
+import base64
+
+dpi = 300
+mat = fitz.Matrix(dpi / 72, dpi / 72)
+highlight_color = (1, 1, 0)
+
 
 def transform_text(input_text):
     # Use urllib.parse to quote the input text
     transformed_text = urllib.parse.quote(input_text)
     return transformed_text
 
+
+def pdf_to_image(filename, page_no, embedd_text=None):
+    filename = transform_text(filename)
+    path = f"https://storage.googleapis.com/medical_docu_pdfs/{filename}"
+    response = requests.get(path)
+    page_no = int(page_no)
+
+    if response.status_code == 200:
+        # Read the content into a BytesIO object
+        pdf_data = BytesIO(response.content)
+        # Open he PDF using PyMuPDF
+        with fitz.open("pdf", pdf_data) as doc:
+            page = doc[page_no]
+
+            text_instances = page.search_for(embedd_text)
+
+            for text_instance in text_instances:
+                highlight = page.add_highlight_annot(text_instance)
+
+                # Set the highlight color
+                highlight.set_colors(stroke=highlight_color)
+
+            image = page.get_pixmap(matrix=mat)
+            image = image.tobytes()
+            image = base64.b64encode(image).decode("utf-8")
+
+            # print(image)
+    return image
+
+
 def contact(filename, page_no):
     filename = transform_text(filename)
-    page_no = str(int(page_no)+1)
+    page_no = str(int(page_no) + 1)
     return f"https://storage.googleapis.com/medical_docu_pdfs/{filename}#page={page_no}"
+
 
 def get_relevant_text(result):
     """
@@ -26,9 +66,12 @@ def get_relevant_text(result):
     context = [
         {
             "pdf_link": contact(x.payload["filename"], x.payload["page_no"]),
+            "pdf_image": pdf_to_image(
+                x.payload["filename"], x.payload["page_no"], x.payload["embeded_text"]
+            ),
             "text": x.payload["text"],
         }
         for x in sorted_result
     ]  # extract title and payload from result
-  
+
     return context
--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-from flask import Flask, request, render_template, session
+from flask import Flask, request, render_template
 import os
 from sentence_transformers import SentenceTransformer
 from qdrant_client import QdrantClient
 from get_relevant_page import get_relevant_text
+import base64
 
 import string
 
 PUNCT_TO_REMOVE = string.punctuation
 app = Flask(__name__)
 
-url = os.environ["URL"]
-api_key = os.environ["API_KEY"]
+url = "https://52ff6a40-646f-4e96-ab95-d19b32ece70d.us-east4-0.gcp.cloud.qdrant.io:6333"
+api_key = "4hxthKyOpwJqXwmwwmEUlVkFSqSr6ETc-s8P8jslW50hGykRxBJ_3g"
 
 client = QdrantClient(
     url=url,
@@ -18,8 +19,9 @@ client = QdrantClient(
     api_key=api_key,
 )
 collection_name = "medical_docu"
-retriever = SentenceTransformer("/app/model/")
-top_k = 10
+retriever = SentenceTransformer("model/")
+top_k = 5
+
 
 @app.route("/")
 def index():
@@ -28,26 +30,24 @@ def index():
 
 @app.route("/search", methods=["POST"])
 def search():
-    try:
-        # Get input text from the form
-        input_text = request.form.get("text")
-        chunks = input_text.lower()
 
-        encoded_query = retriever.encode(
-            chunks
-        ).tolist()  # generate embeddings for the question
+    # Get input text from the form
+    input_text = request.form.get("text")
+    chunks = input_text.lower()
 
-        result = client.search(
-            collection_name=collection_name,
-            query_vector=encoded_query,
-            limit=top_k,
-        )
+    encoded_query = retriever.encode(
+        chunks
+    ).tolist()  # generate embeddings for the question
 
-        data = get_relevant_text(result)
-        print(data)
-        return render_template("index.html", results=data)
-    except Exception as e:
-        return render_template("index.html", error=str(e))
+    result = client.search(
+        collection_name=collection_name,
+        query_vector=encoded_query,
+        limit=top_k,
+    )
+    print(f"results: {result}")
+    data = get_relevant_text(result)
+
+    return render_template("index.html", results=data)
 
 
 if __name__ == "__main__":
